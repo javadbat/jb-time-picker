bbca48cc0c32dd238607296ad7e76fbcad733e60
javadbat <javadbat@gmail.com>
2025-02-01 15:36:19 +0330

    feat: add build config

---
+++ build-config.ts
@@ -0,0 +1,12 @@
+import type { ReactComponentBuildConfig, WebComponentBuildConfig } from "../../tasks/build/builder/src/types.ts";
+
+export const webComponentList: WebComponentBuildConfig[] = [
+  {
+    name: "jb-time-picker",
+    path: "./lib/jb-time-picker.ts",
+    outputPath: "./dist/jb-time-picker.js",
+    external: [],
+    umdName: "JBTimePicker",
+  },
+];
+export const reactComponentList: ReactComponentBuildConfig[] = [];
