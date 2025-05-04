25cdb4ee95e42064288a72b1ed20a1d3522549bc
javadbat <javadbat@gmail.com>
2025-05-04 14:22:16 +0330

    fix ci

--- build-config.ts
+++ build-config.ts
@@ -1,15 +1,15 @@
 import type { ReactComponentBuildConfig, WebComponentBuildConfig } from "../../tasks/build/builder/src/types.ts";
 
 export const webComponentList: WebComponentBuildConfig[] = [
-  // {
-  //   name: "jb-time-picker",
-  //   path: "./lib/jb-time-picker.ts",
-  //   outputPath: "./dist/jb-time-picker.js",
-  //   external: ["jb-core"],
-  //   umdName: "JBTimePicker",
-  //   globals: {
-  //     "jb-core": "JBCore"
-  //   },
-  // },
+  {
+    name: "jb-time-picker",
+    path: "./lib/jb-time-picker.ts",
+    outputPath: "./dist/jb-time-picker.js",
+    external: ["jb-core"],
+    umdName: "JBTimePicker",
+    globals: {
+      "jb-core": "JBCore"
+    },
+  },
 ];
 export const reactComponentList: ReactComponentBuildConfig[] = [];
